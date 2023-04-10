272ff62ba325f5d0cc8702aac6643d6c3c4ffd36
Joao Souza <joaoosouza07@gmail.com>
2023-04-10 20:17:03 -0300

    :lipstick: Styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+
+import GlobalStyle from "./styles/global";
 import RoutesApp from "./routes";
 
 export default function App() {
     return (
         <BrowserRouter>
+            <GlobalStyle />
             <RoutesApp />
         </BrowserRouter>
     );
